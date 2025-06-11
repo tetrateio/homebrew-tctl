f9b3d485c55ebfb76330c0ad154d92cfa0400178
Sergey Marunich <marunich.s@gmail.com>
2025-06-11 12:01:33 -0400

    init tctl-brew

---
+++ Formula/tctl.rb
@@ -0,0 +1,22 @@
+class Tctl < Formula
+  desc "Tetrate Service Bridge Command Line Interface"
+  homepage "https://docs.tetrate.io/service-bridge/reference/cli/guide/"
+  version "1.12.4"
+  license "Commercial"
+
+  if Hardware::CPU.intel?
+    url "https://binaries.dl.tetrate.io/public/raw/versions/darwin-amd64-#{version}/tctl"
+    sha256 "0ec0636c2633fb24d7a829a6a444c7e3ec82c82f80cb4633b0216955d7912bfe" # Intel
+  else
+    url "https://binaries.dl.tetrate.io/public/raw/versions/darwin-arm64-#{version}/tctl"
+    sha256 "ca03442c75e6c7c166c16d7288e2856432647a6c278b8932772aa2a280f72f28" # ARM
+  end
+
+  def install
+    bin.install "tctl"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/tctl version", 2)
+  end
+end
---
+++ README.md
@@ -0,0 +1,17 @@
+# Homebrew Tap for Tetrate Service Bridge CLI (tctl)
+
+This repository contains the Homebrew formula for installing the Tetrate Service Bridge CLI tool (`tctl`).
+
+## Usage
+
+```bash
+# Add this tap to your Homebrew
+brew tap smarunich/tctl-brew
+
+# Install tctl
+brew install tctl
+```
+
+## Documentation
+
+For more information about using `tctl`, please refer to the [official documentation](https://docs.tetrate.io/service-bridge/reference/cli/guide/).
